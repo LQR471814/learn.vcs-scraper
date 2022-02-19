6ee4b6282e48b9d98c0eb5644d17c89941b769b8
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2022-02-18 16:35:15 -0800

    half-baked update

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from datetime import datetime
+import html
 import http.client as http_client
 import logging
+from datetime import datetime
 
 import requests
 from lxml import etree
 
-from utils import normalize_redirect_url, text_without_accessibility, htag_selector, htags
+from utils import htags, normalize_redirect_url, text_without_accessibility
+
 
 def enable_debug_http():
-    #? HTTP Debugging
+    # ? HTTP Debugging
     http_client.HTTPConnection.debuglevel = 1
 
     logging.basicConfig()
@@ -20,48 +22,13 @@ def enable_debug_http():
 
 def get_session(username: str, password: str) -> requests.Session:
     session = requests.Session()
-    login_page_req = session.get(
-        'https://learn.vcs.net/login/index.php',
-        headers={
-            'Host': 'learn.vcs.net',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'DNT': '1',
-            'Connection': 'keep-alive',
-            'Referer': 'https://learn.vcs.net/',
-            'Upgrade-Insecure-Requests': '1',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-User': '?1',
-        }
-    )
+    login_page_req = session.get('https://learn.vcs.net/login/index.php')
 
     tree = etree.HTML(login_page_req.text)
     logintoken = tree.xpath("//input[@name='logintoken']")[0].get('value')
 
     login_post = session.post(
         'https://learn.vcs.net/login/index.php',
-        headers={
-            'Host': 'learn.vcs.net',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': '96',
-            'Origin': 'https://learn.vcs.net',
-            'DNT': '1',
-            'Connection': 'keep-alive',
-            'Referer': 'https://learn.vcs.net/login/index.php',
-            'Upgrade-Insecure-Requests': '1',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-User': '?1',
-        },
         data={
             'anchor': '',
             'logintoken': logintoken,
@@ -75,14 +42,16 @@ def get_session(username: str, password: str) -> requests.Session:
 
     return session
 
+
 def get_lesson_plans(session: requests.Session, class_id: int):
     def get_quarter_screen() -> tuple[str, etree.ElementTree]:
         r = session.get(
             'https://learn.vcs.net/course/view.php',
-            params={ 'id': class_id }
+            params={'id': class_id}
         )
         tree = etree.HTML(r.text)
-        with open('debug', 'w') as f: f.write(r.text)
+        with open('debug', 'w') as f:
+            f.write(r.text)
         return tree.xpath("//a[contains(@title, 'Homework')]")[0].get('href'), tree
 
     quarter_screen_url, quarter_tree = get_quarter_screen()
@@ -109,27 +78,23 @@ def get_lesson_plans(session: requests.Session, class_id: int):
 
     def pick_date(tree: etree.ElementTree) -> str:
         month_map: dict[int, str] = {
-            1: 'January',
-            2: 'February',
-            3: 'March',
-            4: 'April',
-            5: 'May',
-            6: 'June',
-            7: 'July',
-            8: 'August',
-            9: 'September',
-            10: 'October',
-            11: 'November',
-            12: 'December'
+            1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun',
+            7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'
         }
 
-        day_link = tree.xpath(
-            "//li/*[contains(text(), '"
-            f"{month_map[datetime.now().month]} {datetime.now().day}"
-            "')]"
-        )[0]
+        day_elements = tree.xpath(
+            f"//li/a["
+            f"contains(text(), '{month_map[datetime.now().month]}') and "
+            f"contains(text(), '{datetime.now().day}')]"
+        )
 
-        return normalize_redirect_url(r.url, day_link.get('href'))
+        if len(day_elements) < 1:
+            raise Exception("No entree for today.")
+
+        if len(day_elements) > 1:
+            raise Exception("More than one element matched for today's date!")
+
+        return normalize_redirect_url(r.url, day_elements[0].get('href'))
 
     assignment_url = pick_date(dates_tree)
     logging.info(assignment_url)
@@ -142,31 +107,49 @@ def get_lesson_plans(session: requests.Session, class_id: int):
 
         for e in tree.xpath("//div[@class='no-overflow']/*"):
             if e.tag in htags:
-                collecting = 'homework' in ''.join(e.xpath('//text()')).lower()
+                collecting = 'homework' in ''.join(
+                    e.xpath('.//text()')).lower()
                 continue
             if collecting:
                 collection.append(e)
 
         return collection
 
+    def prune_tree(tree: etree._Element) -> etree._Element | None:
+        children = tree.xpath('./*')
+        if len(children) == 0:
+            return None
+        for child in children:
+            if isinstance(child, etree._Element):
+                replaced = prune_tree(child)
+                if replaced is not None:
+                    tree.replace(child, replaced)
+                else:
+                    tree.remove(child)
+            elif isinstance(child, str) and len(child.strip()) == 0:
+                tree.remove(child)
+        return tree
+
+    assignment_text = ""
     for e in pick_homework(assignment_tree):
-        print(etree.tostring(e))
+        assignment_text += etree.tostring(e).decode('utf8') + '\n'
+    assignment_text = html.unescape(assignment_text)
+    print(assignment_text)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
-    #? You get these classes from the 'id' query param in the class homepage url
-    inspect_class_ids = [
-        1154,
-        1446,
-        433,
-        1468,
-        56,
-    ]
+    # ? You get these classes from the 'id' query param in the class homepage url
+    inspect_class_ids = [ 1154, 1446, 433, 1468, 56 ]
 
     f = open('credentials', 'r')
     username, password, *_ = f.read().split('\n')
     print("Using", username, password)
 
     session = get_session(username, password)
-    get_lesson_plans(session, 433)
+    for i in inspect_class_ids:
+        try:
+            get_lesson_plans(session, i)
+        except Exception as e:
+            logging.error(str(e))
