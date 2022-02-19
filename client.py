ada8594869628e0b12b9de9e0b7e1fca80ab6a38
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2022-02-18 20:50:17 -0800

    Added some normalization and structure to the homework entrees

--- client.py
+++ client.py
@@ -1,11 +1,18 @@
 import html
 import logging
+import unicodedata
 
 import requests
 from lxml import etree
 from navigators import *
 
 from utils import htags, prune_tree
+
+
+class UnexpectedHomeworkFormat(Exception):
+    def __init__(self, dump: str) -> None:
+        super().__init__(
+            f"Got an unexpected element whilst structuring homework\nDUMP:\n{dump}")
 
 
 class Client:
@@ -67,10 +74,27 @@ class Client:
 
         return collection
 
-    def homework(self, class_id: int):
+    def homework(self, class_id: int) -> list[str]:
+        assignments: list[str] = []
+
         assignment_tree = self.lesson_plans(class_id)
-        assignment_text = ""
-        for e in self.pick_homework(assignment_tree):
-            assignment_text += etree.tostring(e).decode('utf8') + '\n'
-        assignment_text = html.unescape(assignment_text)
-        return assignment_text
+        homework_tree = self.pick_homework(assignment_tree)
+
+        for e in homework_tree[0]:
+            if e.tag == 'li' and e.text is not None:
+                assignments.append(unicodedata.normalize('NFKD', ''.join(e.itertext())))
+            else:
+                raise UnexpectedHomeworkFormat(self.format_homework_tree(homework_tree))
+
+        return assignments
+
+    def homework_raw(self, class_id: int) -> str:
+        assignment_tree = self.lesson_plans(class_id)
+        homework_tree = self.pick_homework(assignment_tree)
+        return self.format_homework_tree(homework_tree)
+
+    def format_homework_tree(self, tree: list[etree._Element]):
+        homework_text = ""
+        for e in tree:
+            homework_text += etree.tostring(e).decode('utf8') + '\n'
+        return html.unescape(homework_text)
