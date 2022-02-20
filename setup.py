f03effbadaa063d15c9a709ea9f94bc5195a6249
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2022-02-19 16:31:28 -0800

    bump release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='learnvcs',
-    version='0.1.3',
+    version='0.1.4',
     license='GPL-3.0',
     author='LQR471814',
     author_email='bramblefern1013@gmail.com',
